9b4a33d2084da66efdd9055fcc4ca78d5800978d
serioussem <seriousesem@gmail.com>
2022-05-25 14:57:32 +0300

    created unit test

--- app/src/test/java/com/serioussem/currentweather/data/datasource/remote/retrofit/RetrofitDataSourceTest.kt
+++ app/src/test/java/com/serioussem/currentweather/data/datasource/remote/retrofit/RetrofitDataSourceTest.kt
@@ -1,17 +1,19 @@
 package com.serioussem.currentweather.data.datasource.remote.retrofit
 
-import com.google.gson.Gson
+import com.serioussem.currentweather.R
+import com.serioussem.currentweather.data.core.DataResult
 import com.serioussem.currentweather.data.core.InternetConnection
 import com.serioussem.currentweather.data.core.ResourceProvider
 import com.serioussem.currentweather.data.datasource.mappers.ApiModelToDataMapper
 import com.serioussem.currentweather.data.datasource.models.ApiModel
+import com.serioussem.currentweather.data.datasource.models.DataModel
 import com.serioussem.currentweather.data.datasource.models.Main
 import okhttp3.ResponseBody
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
-import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import retrofit2.Response
 
@@ -23,8 +25,14 @@ class RetrofitDataSourceTest {
     private val connectionResultTrue = true
     private val connectionResultFalse = false
     private val testCity = "Львів"
-    private val testResponseSuccess = Response.success(ApiModel(main = Main(temperature = 15.8)))
-    private val testResponseError = Response.error(500, ResponseBody.create(null, "error"))
+    private val testTemperature = 15.8
+    private val testResponseSuccess = Response.success(ApiModel(main = Main(temperature = testTemperature)))
+    private val testResponseError = Response.error<ApiModel>(500, ResponseBody.create(null, "error"))
+    private val testResultSuccess = DataResult.Success(DataModel(city = testCity, temperature = testTemperature))
+    private val testResultErrorCityNotFound = DataResult.Error<DataModel>(message = resourceProvider.string(R.string.city_not_found))
+    private val testResultErrorFailedLoadData = DataResult.Error<DataModel>(message = resourceProvider.string(R.string.failed_to_load_data))
+    private val testResultErrorInternetConnection = DataResult.Error<DataModel>(message = resourceProvider.string(R.string.no_internet_connection_message))
+
 
 
     @Before
@@ -33,10 +41,14 @@ class RetrofitDataSourceTest {
 
     @After
     fun afterMethod() {
+        Mockito.reset(service)
+        Mockito.reset(mapper)
+        Mockito.reset(resourceProvider)
+        Mockito.reset(internetConnection)
     }
 
     @Test
-    suspend fun `test load data if connection is true`() {
+    suspend fun `load data if connection and response is true`() {
         val retrofitDataSource = RetrofitDataSource(
             service = service,
             mapper = mapper,
@@ -46,5 +58,13 @@ class RetrofitDataSourceTest {
         Mockito.`when`(internetConnection.isConnected()).thenReturn(connectionResultTrue)
         Mockito.`when`(service.fetchWeather(testCity)).thenReturn(testResponseSuccess)
 
+        val expected = testResultSuccess
+        val actual = retrofitDataSource.fetchWeather(testCity)
+        Assert.assertEquals(4, 2+2)
+
+    }
+    @Test
+    fun test(){
+        Assert.assertEquals(4, 2+2)
     }
 }
